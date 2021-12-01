cbac80778198866053c62517d0532a721eb612ef
yenanw <yenanw@mail.com>
2021-12-01 11:18:52 +0100

    :bulb: update comments for day 1

--- day01/Day01.hs
+++ day01/Day01.hs
@@ -9,6 +9,8 @@ import Data.Maybe
 day01A :: [Int] -> Int
 day01A = length . catMaybes . filterDecreasing
 
+-- | Given a list of `Int`, compare all elements from left to right
+--   and filter out all numbers that are lower than the previous element.
 filterDecreasing :: [Int] -> [Maybe Int]
 filterDecreasing = filterDecreasing' []
   where
@@ -18,6 +20,8 @@ filterDecreasing = filterDecreasing' []
       where
         acc' = acc ++ [isIncreasing x1 x2]
 
+-- | Given two values, return just the second value if it is bigger,
+--   otherwise Nothing
 isIncreasing :: Ord a => a -> a -> Maybe a
 isIncreasing a b
   | b > a = Just b
@@ -25,12 +29,15 @@ isIncreasing a b
 
 {-===================PART 2===================-}
 
+-- | This just needs some preprossessing of the input
 day01B :: [Int] -> Int
 day01B = day01A . map sumThree . threeMeasurements
 
+-- | Creates a 3-measurement sliding window given a list
 threeMeasurements :: [a] -> [(a, a, a)]
 threeMeasurements xs = zip3 xs (tail xs) (drop 2 xs)
 
+-- | Adds up all numbers in the tuple of 3
 sumThree :: Num a => (a, a, a) -> a
 sumThree (x1, x2, x3) = x1 + x2 + x3
 
