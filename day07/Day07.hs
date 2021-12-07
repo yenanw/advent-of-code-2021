7db7c2ce4c4fb55e0172a1fa20168c127a46be03
yenanw <yenanw@mail.com>
2021-12-07 09:24:29 +0100

    :infinity: add solution for day 7

---
+++ day07/Day07.hs
@@ -0,0 +1,52 @@
+module Day07 where
+
+import           Data.List                      ( partition )
+import           Data.List.Split                ( splitOn )
+
+
+{-===================PART 1===================-}
+
+-- abs (P1 - t) + abs (P2 - t) + abs (P3 - t) + ...
+-- if (Pn - t) < 0  then (Pn - t) = -Pn + t
+-- (P1 + P2 + P3 + ... - Pn) - t * (N - t') where N is the amount of P:s
+
+-- | Equation derived from doing some simple math.
+--   A bit scuffed tho, since it still is linear in complexity because it
+--   takes the absolute value of each crap.
+cost :: Int -> [Int] -> Int
+cost t ns = sum h - sum l - t * (length h - length l)
+  where (l, h) = partition (< t) ns
+
+-- | Given a cost function, map it all element and return the minimum.
+minFuel :: (Int -> [Int] -> Int) -> [Int] -> Int
+minFuel f ns = minimum $ map (`f` ns) [0 .. maximum ns]
+
+day07A :: [Int] -> Int
+day07A = minFuel cost
+
+{-===================PART 2===================-}
+
+-- | The sum of `1+2+3+...+n` is the equal to `(n(n+1))/2`.
+stepCost :: Int -> Int
+stepCost n = n * (n + 1) `div` 2
+
+cost' :: Int -> [Int] -> Int
+cost' t ns = sum ns' where ns' = map (stepCost . (\n -> abs $ n - t)) ns
+
+-- | So even this is kinda brute-forcable? Maybe will optimize later.
+day07B :: [Int] -> Int
+day07B = minFuel cost'
+
+{-===================RESULT===================-}
+
+parse :: String -> [Int]
+parse = map read . splitOn ","
+
+main :: IO ()
+main = do
+  input <- readFile "input.txt"
+  let parsed = parse input
+  putStrLn "==========Part 1=========="
+  print (day07A parsed)
+  putStrLn "==========Part 2=========="
+  print (day07B parsed)
